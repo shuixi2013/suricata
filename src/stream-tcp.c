36d49dbcbbf51d757e3565818318dd4ef1588ef3
Eric Leblond <eric@regit.org>
2015-11-24 21:52:06 +0100

    stream: per TcpStream reassembly depth

--- src/stream-tcp.c
+++ src/stream-tcp.c
@@ -632,6 +632,8 @@ TcpSession *StreamTcpNewSession (Packet *p, int id)
         }
 
         ssn->state = TCP_NONE;
+        ssn->client.reassembly_depth = stream_config.reassembly_depth;
+        ssn->server.reassembly_depth = stream_config.reassembly_depth;
         ssn->flags = stream_config.ssn_init_flags;
         ssn->tcp_packet_flags = p->tcph ? p->tcph->th_flags : 0;
 
@@ -5764,6 +5766,14 @@ int StreamTcpSegmentForEach(const Packet *p, uint8_t flag, StreamSegmentCallback
         cnt++;
     }
     return cnt;
+}
+
+void StreamTcpSetReassemblyDepth(TcpSession *ssn, uint32_t size)
+{
+    ssn->server.reassembly_depth = size;
+    ssn->client.reassembly_depth = size;
+
+    return;
 }
 
 #ifdef UNITTESTS
